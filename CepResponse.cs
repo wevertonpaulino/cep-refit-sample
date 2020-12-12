fc27ba13a9c2fb5ce5ebc70777df38d37cade3b4
Weverton Paulino <wevertoncesar@gmail.com>
2020-12-11 21:46:27 -0300

    Add response class

---
+++ CepResponse.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace CepRefitSample
+{
+    public class CepResponse
+    {
+        [JsonProperty("status")]
+        public string Status { get; set; }
+        [JsonProperty("code")]
+        public string Code { get; set; }
+        [JsonProperty("state")]
+        public string State { get; set; }
+        [JsonProperty("city")]
+        public string City { get; set; }
+        [JsonProperty("district")]
+        public string District { get; set; }
+        [JsonProperty("address")]
+        public string Address { get; set; }
+    }
+}
