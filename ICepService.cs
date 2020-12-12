586d557f9a5a54fabfd4d6594563cd0d0a4f94d6
Weverton Paulino <wevertoncesar@gmail.com>
2020-12-11 21:56:41 -0300

    Add specification interface

---
+++ ICepService.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace CepRefitSample
+{
+    public interface ICepService
+    {
+        [Get("/cep/{cep}.json")]
+        public Task<CepResponse> GetAsync(string cep);
+    }
+}
