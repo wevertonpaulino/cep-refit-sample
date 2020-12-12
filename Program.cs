c4e1cfdfd63009a0e02a3c5995fa402b4f783c60
Weverton Paulino <wevertoncesar@gmail.com>
2020-12-11 22:12:21 -0300

    Implements api call

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
 
 namespace CepRefitSample
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = RestService.For<ICepService>("https://ws.apicep.com/");
+            var cep = "85875000";
+
+            Console.WriteLine("Searching for information about the zip code: " + cep);
+
+            var response = await client.GetAsync(cep);
+
+            Console.WriteLine($"\nStatus: {response.Status}" +
+                              $"\nCode: {response.Code}" +
+                              $"\nState: {response.State}" +
+                              $"\nCity: {response.City}" +
+                              $"\nDistrict: {response.District}" +
+                              $"\nAddress: {response.Address}");
+
+            Console.ReadKey();
         }
     }
 }
